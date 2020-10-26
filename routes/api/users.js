fbabd6832f02d41eae66b34dff957f91111558fb
malrajmund <malrajmund@gmail.com>
2020-10-26 19:37:38 +0100

    Added jwt token

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { restart } = require('nodemon');
+const config = require('config');
 
 const User = require('../../models/User')
 
@@ -30,7 +32,7 @@ async (req, res) => {
     const { firstName, lastName, email, province, town, street, streetNumber, postCode, phoneNumber, password } = req.body;
 
     try{
-        let user = await User.findOne({email});
+        let user = await User.findOne({ email });
         if(user){
             return res
                 .status(400)
@@ -57,12 +59,23 @@ async (req, res) => {
         })
 
         const salt = await bcrypt.genSalt(10);
-
         user.password = await bcrypt.hash(password, salt);
-
         await user.save();
 
-        res.send('User registered');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token });
+            }); 
 
     }   catch(err){
         console.error(err.message);
@@ -70,9 +83,6 @@ async (req, res) => {
             .status(500)
             .send('Server error');
     }
-
-
-    
 });
 
 module.exports = router;
