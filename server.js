2e9e499580ae25c113eee683023cfb80c7a20379
Rajmund Malec <46901411+malrajmund@users.noreply.github.com>
2021-03-12 11:12:25 +0100

    npm packages

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const connectDB = require('./config/db');
-
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
 const app = express();
 
 //Połącz z bazą danych
 connectDB();
 
 //Inicjalizacja Middleware
-app.use(express.json({ extended: false }));
+app.use(express.json({ extended: false, limit: "50mb" }));
+app.use(cors());
 
-app.get('/', (req, res) => res.send('API Running'));
+app.get("/", (req, res) => res.send("API Running"));
 
 //Stworzenie routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/offers', require('./routes/api/offers'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/offers", require("./routes/api/offers"));
+app.use("/api/chats", require("./routes/api/chats"));
+app.use("/api/categories", require("./routes/api/categories"));
 
 
 //Nasłuchiwanie na porcie 5000 lub porcie po deployu
